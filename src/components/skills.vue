a740b6ea1f8cc6a27e8d00227616a636befeed24
Mila Endo <milal.alim@gmail.com>
2018-07-03 10:20:50 -0700

    style/ input and method for adding new skill to the skills array

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="Skills">
-    <h1> {{name}} </H1>
-
-      <button @click="changeName" :disabled="btnState">Change Name</button>
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you have.." v-model="skill">
+        {{ skill }}
+      </form>
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+      </ul>
+      <p>These are the skills that you posses.</p>
+    </div>
   </div>
 </template>
 
@@ -11,8 +18,58 @@ export default{
   name: 'Skills',
   data() {
     return {
-      name: "Mila"
+      skill: '',
+      skills: [
+        {"skill": 'Vue.js'},
+        {"skill": 'Frontend Developer'}
+      ],
+    }
+  },
+  methods: {
+    addSkill(){
+      this.skills.push({skill: this.skill})
+      this.skill = ''
     }
   }
 }
 </script>
+
+<style scoped>
+.holder {
+  background: #fff;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #E0EDF4;
+  border-left: 5px solid #3EB3F6;
+  margin-bottom: 2px;
+  color: #3E5252;
+}
+
+p {
+  text-align:center;
+  padding: 30px 0;
+  color: gray;
+}
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687F7F;
+}
+</style>
